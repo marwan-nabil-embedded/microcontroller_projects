350163be38473d9373c25240a11df5db8eabef69
Marwan_Nabil <141582334+marwan-nabil-embedded@users.noreply.github.com>
2024-04-30 13:13:45 +0300

    show_alphabetic_char_on_7_seg.c

--- show_alphabetic_char_on_7_seg.c
+++ show_alphabetic_char_on_7_seg.c
@@ -1,11 +1,11 @@
 #define pin_number 8
-
+int numbers[8]={1,3,15,13,12,14,4,5};
 
 /*********************************/
 void setup() {
   
   for (int pin = 0; pin <= pin_number; pin++) {
-    pinMode(pin, OUTPUT); 
+    pinMode(numbers[pin], OUTPUT); 
   }
   
 }
@@ -14,91 +14,91 @@ void setup() {
 void letter_A() { //most bit(pin 7) 01110111 least bit (pin0)
 short letter_A_binary[pin_number]={0,1,1,1,0,1,1,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_A_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_A_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_B() { //most bit(pin 7) 01111111 least bit (pin0)
 short letter_B_binary[pin_number]={0,1,1,1,1,1,1,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_B_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_B_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_C() { //most bit(pin 7) 00111001 least bit (pin0)
 short letter_C_binary[pin_number]={0,0,1,1,1,0,0,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_C_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_C_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_D() { //most bit(pin 7) 01011110 least bit (pin0)
 short letter_D_binary[pin_number]={0,1,0,1,1,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_D_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_D_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_E() { //most bit(pin 7) 01111001 least bit (pin0)
 short letter_E_binary[pin_number]={0,1,1,1,1,0,0,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_E_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_E_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_F() { //most bit(pin 7) 01110001 least bit (pin0)
 short letter_F_binary[pin_number]={0,1,1,1,0,0,0,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_F_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_F_binary[(pin_number-1)-i]);  
 }
 } 
 /************************************************************************************/
 void letter_G() { //most bit(pin 7) 10000000 least bit (pin0)  cant represent it
 short letter_G_binary[pin_number]={1,0,0,0,0,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_G_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_G_binary[(pin_number-1)-i]);  
 }
 } 
 /************************************************************************************/
 void letter_H() { //most bit(pin 7) 01110110 least bit (pin0)
 short letter_H_binary[pin_number]={0,1,1,1,0,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_H_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_H_binary[(pin_number-1)-i]);  
 }
 } 
 /************************************************************************************/
 void letter_I() { //most bit(pin 7) 00000110 least bit (pin0)
 short letter_I_binary[pin_number]={0,0,0,0,0,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_I_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_I_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_J() { //most bit(pin 7) 00011110 least bit (pin0)
 short letter_J_binary[pin_number]={0,0,0,1,1,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_J_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_J_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_K() { //most bit(pin 7) 10000000 least bit (pin0)  cant represent it
 short letter_K_binary[pin_number]={1,0,0,0,0,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_K_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_K_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_L() { //most bit (pin 7) 00111000 least bit (pin0)  
 short letter_L_binary[pin_number]={0,0,1,1,1,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_L_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_L_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_M() { //most bit (pin 7) 10000000 least bit (pin0)  cant represent it
 short letter_M_binary[pin_number]={1,0,0,0,0,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_M_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_M_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
@@ -106,91 +106,91 @@ digitalWrite(i, letter_M_binary[(pin_number-1)-i]);
 void letter_N() { //most bit (pin 7) 10000000 least bit (pin0)  cant represent it
 short letter_N_binary[pin_number]={1,0,0,0,0,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_N_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_N_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_O() { //most bit (pin 7) 01011100 least bit (pin0)  
 short letter_O_binary[pin_number]={0,1,0,1,1,1,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_O_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_O_binary[(pin_number-1)-i]);  
 }
 } 
 /************************************************************************************/
 void letter_P() { //most bit (pin 7) 01110011 least bit (pin0)
 short letter_P_binary[pin_number]={0,1,0,1,1,1,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_P_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_P_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_Q() { //most bit (pin 7) 01100111 least bit (pin0)
 short letter_Q_binary[pin_number]={0,1,1,0,0,1,1,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_Q_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_Q_binary[(pin_number-1)-i]);  
 }
 } //
 /************************************************************************************/
 void letter_R() { //most bit (pin 7) 10000000 least bit (pin0)  cant represent it
 short letter_R_binary[pin_number]={1,0,0,0,0,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_R_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_R_binary[(pin_number-1)-i]);  
 }
 }//
 /************************************************************************************/
 void letter_S() { //most bit (pin 7) 01101101 least bit (pin0) 
 short letter_S_binary[pin_number]={0,1,1,0,1,1,0,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_S_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_S_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_T() { //most bit (pin 7) 01111000 least bit (pin0) 
 short letter_T_binary[pin_number]={0,1,1,1,1,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_T_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_T_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_U() { //most bit (pin 7) 00111110 least bit (pin0) 
 short letter_U_binary[pin_number]={0,0,1,1,1,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_U_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_U_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_V() { //most bit (pin 7) 00111110 least bit (pin0) 
 short letter_V_binary[pin_number]={0,0,1,1,1,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_V_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_V_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_W() { //most bit (pin 7) 10000000 least bit (pin0) 
 short letter_W_binary[pin_number]={1,0,0,0,0,0,0,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_W_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_W_binary[(pin_number-1)-i]);  
 }
 }//01110110
 /************************************************************************************/
 void letter_X() { //most bit (pin 7) 01110110 least bit (pin0) 
 short letter_X_binary[pin_number]={0,1,1,1,0,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_X_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_X_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
 void letter_Y() { //most bit (pin 7) 01101110 least bit (pin0) 
 short letter_Y_binary[pin_number]={0,1,1,0,1,1,1,0};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_Y_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_Y_binary[(pin_number-1)-i]);  
 }
 }//01011011
 /************************************************************************************/
 void letter_Z() { //most bit (pin 7) 01011011 least bit (pin0) 
 short letter_Z_binary[pin_number]={0,1,0,1,1,0,1,1};
 for(short i=0;i<pin_number;i++){
-digitalWrite(i, letter_Z_binary[(pin_number-1)-i]);  
+digitalWrite(numbers[i], letter_Z_binary[(pin_number-1)-i]);  
 }
 }
 /************************************************************************************/
@@ -207,8 +207,7 @@ void loop() {
   for(short y=0;y<26;y++){
     function_ptr[y] ();
     delay(2000);
-    for(short k=0;k<8;k++){
-        digitalWrite(k,0);
-    }
+    
+    
   }
 }
