98f870cf5b39248abc9f6730f7b56e8cce9a83b5
Marwan_Nabil <141582334+marwan-nabil-embedded@users.noreply.github.com>
2024-05-01 10:14:17 +0300

    show_alphabetic_char_and_numbers_on_7_seg.c

--- show_alphabetic_char_on_7_seg.c
+++ show_alphabetic_char_on_7_seg.c
@@ -10,6 +10,77 @@ void setup() {
   
 }
 /********************************/
+/*0,1,2,3,4,5,6,7,8,9*/
+void num_0(){ //00111111
+short num_0_binary[pin_number]={0,0,1,1,1,1,1,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_0_binary[(pin_number-1)-i]);  
+}
+}
+/*********************************/
+void num_1(){ //00000110
+short num_1_binary[pin_number]={0,0,0,0,0,1,1,0};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_1_binary[(pin_number-1)-i]);  
+}
+}
+/*********************************/
+void num_2(){ //01011011
+short num_2_binary[pin_number]={0,1,0,1,1,0,1,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_2_binary[(pin_number-1)-i]);  
+}
+}
+/*******************************/
+void num_3(){ //01001111
+short num_3_binary[pin_number]={0,1,0,0,1,1,1,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_3_binary[(pin_number-1)-i]);  
+}
+}
+/*******************************/
+void num_4(){ //01100110
+short num_4_binary[pin_number]={0,1,1,0,0,1,1,0};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_4_binary[(pin_number-1)-i]);  
+}
+}
+/*********************************/
+void num_5(){ //01101101
+short num_5_binary[pin_number]={0,1,1,0,1,1,0,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_5_binary[(pin_number-1)-i]);  
+}
+}
+/********************************/
+void num_6(){ //01111101
+short num_6_binary[pin_number]={0,1,1,1,1,1,0,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_6_binary[(pin_number-1)-i]);  
+}
+}
+/*********************************/
+void num_7(){ //00000111
+short num_7_binary[pin_number]={0,0,0,0,0,1,1,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_7_binary[(pin_number-1)-i]);  
+}
+}
+/********************************/
+void num_8(){ //01111111
+short num_8_binary[pin_number]={0,1,1,1,1,1,1,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_8_binary[(pin_number-1)-i]);  
+}
+}
+/********************************/
+void num_9(){ //01101111
+short num_9_binary[pin_number]={0,1,1,0,1,1,1,1};
+for(short i=0;i<pin_number;i++){
+digitalWrite(numbers[i], num_9_binary[(pin_number-1)-i]);  
+}
+}
+/********************************/
 /*A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, and Z*/
 void letter_A() { //most bit(pin 7) 01110111 least bit (pin0)
 short letter_A_binary[pin_number]={0,1,1,1,0,1,1,1};
@@ -185,7 +256,7 @@ short letter_Y_binary[pin_number]={0,1,1,0,1,1,1,0};
 for(short i=0;i<pin_number;i++){
 digitalWrite(numbers[i], letter_Y_binary[(pin_number-1)-i]);  
 }
-}//01011011
+}
 /************************************************************************************/
 void letter_Z() { //most bit (pin 7) 01011011 least bit (pin0) 
 short letter_Z_binary[pin_number]={0,1,0,1,1,0,1,1};
@@ -194,7 +265,8 @@ digitalWrite(numbers[i], letter_Z_binary[(pin_number-1)-i]);
 }
 }
 /************************************************************************************/
- void (*function_ptr[26])(void) = {
+ void (*function_ptr[36])(void) = {
+                                    num_0,num_1,num_2,num_3,num_4,num_5,num_6,num_7,num_8,num_9,
                                     letter_A, letter_B, letter_C,letter_D,letter_E,letter_F,letter_G,
                                     letter_H,letter_I,letter_J,letter_K,letter_L,letter_M,letter_N,
                                     letter_O,letter_P,letter_Q,letter_R,letter_S,letter_T,letter_U,
@@ -204,10 +276,8 @@ digitalWrite(numbers[i], letter_Z_binary[(pin_number-1)-i]);
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(short y=0;y<26;y++){
+  for(short y=0;y<36;y++){
     function_ptr[y] ();
-    delay(2000);
-    
-    
+    delay(1500);
   }
 }
